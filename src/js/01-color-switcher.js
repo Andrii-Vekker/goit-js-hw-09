79484d915f5d15b04fc7f7acce15e11981083af1
Andrii-Vekker <vekker304@gmail.com>
2022-07-30 23:40:35 +0300

    hernya

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -11,7 +11,7 @@ refs.btnStop.addEventListener("click", stopChangeColor);
 refs.body.style.display = "flex"
 refs.body.style.justifyContent = "center"
 refs.body.style.marginTop = "150px"
-refs.p.style.transform = "translate(-700px, -100px)"
+refs.p.style.transform = "translate(-100%, -100%)"
 refs.btnStart.style.marginRight = "10px"
 refs.btnStart.style.width = "80px"
 refs.btnStop.style.width = "80px"
@@ -41,3 +41,35 @@ function stopChangeColor() {
     refs.btnStop.style.color = "green"
     refs.btnStart.disabled = false;
 };
+
+//////////////second
+// const startBtn = document.querySelector('button[data-start]');
+// const stopBtn = document.querySelector('button[data-stop]');
+// const btnContainer = document.querySelector('.container');
+// const body = document.querySelector('body');
+// let timerId = null;
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// btnContainer.addEventListener('click', onClickStartColorChange);
+
+// function onClickStartColorChange(event) {
+//   clearInterval(timerId);
+//   const currentEl = event.target;
+
+//   chaeckDisableStatus(currentEl, stopBtn);
+
+//   timerId = setInterval(() => {
+//     body.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+// }
+
+function chaeckDisableStatus(disabledEl, activeEl) {
+  if (disabledEl.disabled === true) {
+    return;
+  } else if (activeEl.disabled === false) {
+    activeEl.disabled === true;
+  }
+}
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,153 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+
+// const refs = {
+//     input: document.querySelector("#datetime-picker"),
+//     btnStart: document.querySelector("[data-start]"),
+//     timerValue: document.querySelector(".value")
+// };
+
+// refs.btnStart.addEventListener("click", startTimer)
+
+// refs.btnStart.disabled = true
+// let deadline = null;
+// console.log(deadline)
+// const currentTime = new Date().getTime();
+// // console.log(currentTime)
+// const deltaTime = deadline - currentTime;
+//    console.log(deltaTime)
+// const options = {
+//     enableTime: true,
+//     time_24hr: true,
+//     defaultDate: new Date(),
+//     minuteIncrement: 1,
+//     onClose(selectedDates) {
+//         if (deadline <= currentTime) {
+//             alert("Please choose a date in the future");
+//         }
+        
+//         deadline = selectedDates[0];
+//             refs.btnStart.disabled = false
+//             return deadline
+//         },
+        
+//     };
+
+
+// const fp = flatpickr(refs.input, options);
+
+
+
+
+
+
+
+// function initialTimer({days, hours, minutes, seconds}) {
+//     refs.timerValue.textContent = `${days}:${hours}:${minutes}:${seconds}`
+// }
+
+// function startTimer(e) {
+//     setInterval(() => {
+//         const timeValue = convertMs(deltaTime)
+//         console.log(timeValue)
+//     }, 1000)
+// }
+
+
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = addLeadingZero(Math.floor(ms / day));
+//   // Remaining hours
+//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//   // Remaining minutes
+//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//   // Remaining seconds
+//   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+//   return { days, hours, minutes, seconds };
+// };
+
+// function addLeadingZero(value) {
+//     return String(value).padStart(2, "0")
+
+// };
+
+///////////////////////////////////////////////////
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('[data-start]'),
+  timerValue: document.querySelector('.value'),
+};
+
+refs.btnStart.addEventListener('click', startTimer);
+
+refs.btnStart.disabled = true;
+
+let deadline = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    deadline = selectedDates[0].getTime();
+    const d = deadline - Date.now();
+    console.log(deadline);
+    if (d <= 0) {
+      alert('Please choose a date in the future');
+    }
+
+    refs.btnStart.disabled = false;
+    // return deadline
+  },
+};
+
+const fp = flatpickr(refs.input, options);
+console.log();
+
+function initialTimer({ days, hours, minutes, seconds }) {
+  refs.timerValue.textContent = `${days}:${hours}:${minutes}:${seconds}`;
+}
+
+function startTimer(e) {
+  setInterval(() => {
+    const timeValue = convertMs(deltaTime);
+    console.log(timeValue);
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
